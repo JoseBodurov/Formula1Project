4f49a5725e50b9b04de07ebc4ccd0d07a68dccfd
JoseBodurov <josetorodrigo9@gmail.com>
2024-01-31 22:10:39 +0200

    Validation Dependency added

--- src/main/java/bg/softuni/formula1attempt1/models/dto/AddRaceBindingModel.java
+++ src/main/java/bg/softuni/formula1attempt1/models/dto/AddRaceBindingModel.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -13,8 +16,10 @@ public class AddRaceBindingModel {
 
     private String raceName;
 
+    @Size(min = 5, message = "Description length must be more than 5 characters")
     private String experienceDescription;
 
+    @Pattern(regexp = "https:\\/\\/www\\.youtube\\.com\\/watch\\?v=.*", message = "Invalid youtube url provided")
     private String videoUrl;
 
 }
